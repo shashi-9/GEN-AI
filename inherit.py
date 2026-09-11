1003ddde311116a855eeb6b01e8947a042c6aa9b
shashi-9 <shashi09sk11@gmail.com>
2026-09-11 12:37:04 +0530

    Add files via upload
    new file

---
+++ inherit.py
@@ -0,0 +1,60 @@
+# # # class father:
+# # #     def __init__(self,a):
+# # #         self.a=a
+# # #     def eat(self):
+# # #         print('eating {self.a}')
+# # # class mother:
+# # #     def __init__(self, b):
+# # #         self.b=b
+# # #     def read(self):
+# # #         print("reading book")
+# # # class child(father,mother):
+# # #     def __init__(self,a,b,c):
+# # #         father.__init__(self, a)
+# # #         mother.__init__(self, b)
+# # #         self.c=c
+# # #     def write(self):
+# # #         print(self.c)
+# # # obj=child('eat, read, write')
+# # class elementary:
+# #     #def __init__(self, sname,Slocation):
+# #         #self.sname=sname
+# #         #self.Slocation=Slocation
+# #     def study(self):
+# #         print(f'studying 1st class')
+# #         #print('studying at {self.Slocation}')
+# # class university(elementary):
+# #     def study(self):
+# #         print('im studying Btech')
+# #         x=[elementary,university]
+# class vehicle:
+#     def __init__(self,brand,model):
+#         self.brand =brand
+#         self.model =model
+#     def move(self):
+#         print('move')
+# class car(vehicle):
+#    pass
+# class boat(vehicle):
+#     def move(self):
+#         print('sail')
+# class plane(vehicle):
+#     def move(self):
+#         print('fly')
+# car = car('BMW, Mahendra')
+# boat = boat('touring, cargo')
+# plane =plane('boeing, passenger')
+# for x in (car,boat,plane):
+#     print(x.brand)
+    
+#     x.move()
+class Student:
+    def __init(self,name, grade):
+        self.name=name
+        self.grade=grade
+    def display_info(self):
+        print(f'student: {self.name}, grade:{self.grade}')
+    Student1 = Student("james", 'A')
+    Student1.display_info()
+    Student1.grade = 'A+'
+    print(Student1.grade)
